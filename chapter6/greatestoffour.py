2759c3d00b37a47d1e2d23556fe3c0ec3d6e9b28
arunbhalli <tomarbhalli@gmail.com>
2022-10-05 23:57:59 +0200

    adds solution for finding greater no

---
+++ chapter6/greatestoffour.py
@@ -0,0 +1,22 @@
+a =int(input('enter your no'))
+b= int(input('enter your no'))
+c= int(input('enter your no'))
+d=int(input('enter your no'))
+
+if(a > b):
+  f1= a
+else:
+  f1= b
+  
+if(f1 > c):
+  f2 = f1
+else:
+  f2= c
+
+if(f2 > d):
+  f3 = f2
+  print('greater no is',f3)
+else:
+  f3= d
+  print('greater no is',f3)
+  
