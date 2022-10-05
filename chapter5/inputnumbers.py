7aefd459af27481e1fd5d231cc723b18935a9f37
arunbhalli <tomarbhalli@gmail.com>
2022-10-05 19:29:04 +0200

    adds solution for print special numbers onse only repeated word only

---
+++ chapter5/inputnumbers.py
@@ -0,0 +1,12 @@
+s1=int(input('Enter marks of student 1:\n '))
+s2=int(input('Enter marks of student 2: \n'))
+s3=int(input('Enter marks of student 3:\n '))
+s4=int(input('Enter marks of student 4: \n'))
+s5=int(input('Enter marks of student 5: \n'))
+s6=int(input('Enter marks of student 6: \n'))
+s7=int(input('Enter marks of student 7:\n '))
+s8=int(input('Enter marks of student 8: \n'))
+res = {
+  s1,s2,s3,s4,s5,s6,s7,s8
+}
+print(res)
