32d1f5e877ac5693f344300e783fdca184e4f50b
arunbhalli <tomarbhalli@gmail.com>
2022-10-05 18:00:40 +0200

    adds functionality for sorting list of students data

---
+++ chapter4/studentsList.py
@@ -0,0 +1,9 @@
+s1=int(input('Enter marks of student 1: '))
+s2=int(input('Enter marks of student 2: '))
+s3=int(input('Enter marks of student 3: '))
+s4=int(input('Enter marks of student 4: '))
+s5=int(input('Enter marks of student 5: '))
+s6=int(input('Enter marks of student 6: '))
+sl =[s1,s2,s3,s4,s5,s6]
+sl.sort()
+print(sl)
