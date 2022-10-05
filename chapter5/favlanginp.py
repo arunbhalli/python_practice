ae7cef5c873883bcbc32279f358a266f95280986
arunbhalli <tomarbhalli@gmail.com>
2022-10-05 23:05:50 +0200

    input from friends for there fav lang

---
+++ chapter5/favlanginp.py
@@ -0,0 +1,13 @@
+facLang={}
+a= input('enter your favourite language Arun:')
+b = input('enter your favourite language Raju:')
+c = input('enter your favourite language Rahul:')
+d= input('enter your favourite language Mohit:')
+
+facLang={
+  'Arun': a,
+  'Raju': b,
+  'Rahul':c,
+  'Mohit':d
+}
+print(facLang)
