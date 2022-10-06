6b08a835d5bc8f57bd663d11d79d53105511df74
arunbhalli <tomarbhalli@gmail.com>
2022-10-06 09:31:16 +0200

    adds conditions for checking len of input user name

---
+++ chapter6/lengthofuserName.py
@@ -0,0 +1,7 @@
+userName = input('User Name:-')
+lenofuName = len(userName)
+
+if (lenofuName < 10):
+    print('it contains less than 10 character')
+else:
+    print('it contains more than 10 character')
