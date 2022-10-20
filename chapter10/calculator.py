9b58ec4117a10ac3b46416828b46a06e3395e2d9
arunbhalli <tomarbhalli@gmail.com>
2022-10-20 18:55:28 +0200

    adds solution for calculatar q2

---
+++ chapter10/calculator.py
@@ -0,0 +1,18 @@
+import math
+class  Calculator():
+  def __init__(self,number):
+    self.number = number
+    
+  def square(self):
+    return self.number * self.number
+  
+  def cube(self):
+    return self.number * self.number*self.number
+  def squareroot(self):
+    return math.sqrt(self.number)
+    
+two = Calculator(2)
+print(two.number)
+print(two.square())
+print(two.cube())
+print(two.squareroot())
